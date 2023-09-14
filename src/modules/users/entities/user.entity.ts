b58eae8a88a44f5f646a0358c5b7ed317e5c22f7
JonathanVCB <viniciusjonathan@hotmail.com>
2023-09-14 15:44:30 -0300

    start project, and add user entity

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,15 @@
+import { randomUUID } from 'node:crypto';
+
+export class User {
+  readonly id: string;
+  name: string;
+  email: string;
+  birthDate: string;
+  bio: string;
+  createdAt: Date;
+
+  constructor() {
+    this.id = randomUUID();
+    this.createdAt = new Date();
+  }
+}
