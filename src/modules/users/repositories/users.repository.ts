f4cf8cc08b5058847b9cabaa66b838b927d32b37
JonathanVCB <viniciusjonathan@hotmail.com>
2023-09-14 16:08:44 -0300

    add user repository

--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -1,7 +1,11 @@
 import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
 import { User } from '../entities/user.entity';
 
 export abstract class UsersRepository {
   abstract create(data: CreateUserDto): Promise<User> | User;
-  //teste
+  abstract findAll(): Promise<User[]> | [];
+  abstract findOne(id: string): Promise<User> | User;
+  abstract update(id:string, data:UpdateUserDto): Promise<User> | User;
+  abstract delete(id: string): Promise<void> | void
 }
