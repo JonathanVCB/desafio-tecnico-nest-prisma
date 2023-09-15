150ae78836ffeba81d3a45b0a4e8313613990b70
JonathanVCB <viniciusjonathan@hotmail.com>
2023-09-15 00:46:03 -0300

    add post entity

--- src/modules/posts/entities/post.entity.ts
+++ src/modules/posts/entities/post.entity.ts
@@ -1,1 +1,14 @@
-export class Post {}
+import { randomUUID } from 'crypto';
+import { User } from 'src/modules/users/entities/user.entity';
+
+export class Post {
+  readonly id: string;
+  content: string;
+  user: User;
+  userId: string;
+  readonly createdAt: Date;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
